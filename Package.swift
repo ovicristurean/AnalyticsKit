cbc6dbd286e2d42004e024ab05558e8ebe9e71c5
ovicristurean <cristurean_ovidiu@yahoo.com>
2026-02-23 14:16:02 +0200

    add .zip with xcframework, and Package.swift for release 0.1.0

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "analyticsKit",
             url: "https://github.com/ovicristurean/AnalyticsKit/releases/download/0.1.0/analyticsKit.xcframework.zip",
-            checksum: "aec9fd24ae02408bfb9eed2f6cb13c285b234c9ffd0072da0c38483063247b5f"
+            checksum: "cc14e57939bfb246694342e8d3159529736e9c59b0aa5c5327a5d2c5ca05fe53"
         )
     ]
 )
